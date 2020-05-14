f610135f8f723d84aafef41efcd72a719d07af2b
janilaunonen <48886257+janilaunonen@users.noreply.github.com>
2020-05-14 21:43:15 +0300

    Added some comments for testing purposes

--- test.s
+++ test.s
@@ -1,8 +1,8 @@
-.org $600
-lda $d01f
-and #$07
+.org $600    ; start address for check-exit-condition routine
+lda $d01f    ; load console button status
+and #$07     ; start-select-option
 cmp #$07
-bne exit
-rts
-exit: brk
+bne exit     ; if any of the console buttons are pressed, then jump to exit
+rts          ; return
+exit: brk    ; break to debugger
 
